6f5ca710dcb10f6134447633a56739ba7e40ebe5
Santeri Vesalainen <santeri.vesalainen@gmail.com>
2025-03-29 13:34:05 +0200

    Update postcss.config.mjs

--- site/postcss.config.mjs
+++ site/postcss.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
 
 const config = {
-  plugins: [tailwindcss(), autoprefixer()]
+  plugins: [
+    tailwindcss(),
+    autoprefixer()
+  ]
 };
 
 export default config;
